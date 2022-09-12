05df905debc84165c78b405a31acc84357cfdcee
Justin <justinrkinsman@gmail.com>
2022-09-12 10:24:06 -0300

    Add CSS styling to heading

--- wheres-waldo/src/components/Characters.js
+++ wheres-waldo/src/components/Characters.js
@@ -3,13 +3,17 @@ import React from 'react'
 import Grimes from '../images/grimes.jpg'
 import Moe from '../images/moe.jpg'
 import Poochie from '../images/poochie.jpg'
+import './Characters.css'
 
 export const Characters = () => {
     return (
-        <>
-            <CharacterCards source={Grimes} alt={'Frank Grimes'}/>
-            <CharacterCards source={Moe} alt={'Moe Szyslak'}/>
-            <CharacterCards source={Poochie} alt={'Poochie'}/>
-        </>
+        <div className='characterHeader'>
+            <p>Find these characters</p>
+            <div className="charactersToFind">
+                <CharacterCards source={Grimes} alt={'Frank Grimes'}/>
+                <CharacterCards source={Moe} alt={'Moe Szyslak'}/>
+                <CharacterCards source={Poochie} alt={'Poochie'}/>
+            </div>
+        </div>
     )
 }
