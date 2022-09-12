266f33e6bdd1e047e46bf7a4c17527f74f1996e8
Justin <justinrkinsman@gmail.com>
2022-09-12 10:34:50 -0300

    Change color of title text

--- wheres-waldo/src/components/Characters.js
+++ wheres-waldo/src/components/Characters.js
@@ -4,11 +4,12 @@ import Grimes from '../images/grimes.jpg'
 import Moe from '../images/moe.jpg'
 import Poochie from '../images/poochie.jpg'
 import './Characters.css'
+import Title from '../images/title.png'
 
 export const Characters = () => {
     return (
         <div className='characterHeader'>
-            <p>Find these characters</p>
+            <img src={Title} alt='Find These Characters'></img>
             <div className="charactersToFind">
                 <CharacterCards source={Grimes} alt={'Frank Grimes'}/>
                 <CharacterCards source={Moe} alt={'Moe Szyslak'}/>
