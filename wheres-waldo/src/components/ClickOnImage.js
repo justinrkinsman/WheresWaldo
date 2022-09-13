38e80eca6f05563fd2e979570154cea82fb6150c
Justin <justinrkinsman@gmail.com>
2022-09-13 09:38:57 -0300

    Character Selector changes visibility on click

--- wheres-waldo/src/components/ClickOnImage.js
+++ wheres-waldo/src/components/ClickOnImage.js
@@ -1,22 +1,25 @@
 //Determine if character selector is open
-const IsCharacterSelectorOpen = () => {
-    if (document.getElementById('CharacterSelector') === null) {
-        OpenCharacterSelector()
+const IsCharacterSelectorOpen = (e) => {
+    let box = document.getElementById('CharacterSelector')
+    if (box.style.visibility === 'hidden') {
+        OpenCharacterSelector(e)
     }else{
-        CloseCharacterSelector()
+        CloseCharacterSelector(e)
     }
 }
 
 //If character selector is closed:
-const OpenCharacterSelector = () => {
-    console.log('Open')
+const OpenCharacterSelector = (e) => {
+    let box = document.getElementById('CharacterSelector')
+    box.style.visibility = 'visible'
 }
 
 //If character selector is open
-const CloseCharacterSelector = () => {
-    console.log('Close')
+const CloseCharacterSelector = (e) => {
+    let box = document.getElementById('CharacterSelector')
+    box.style.visibility = 'hidden'
 }
 
 export const ClickOnImage = e => {
-    IsCharacterSelectorOpen()
+    IsCharacterSelectorOpen(e)
 }
