e9d178e576c545e5b3f8d072bc0086e8232ddd17
Justin <justinrkinsman@gmail.com>
2022-09-19 09:30:41 -0300

    Add firebase to project

---
+++ wheres-waldo/src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDWSX8mJCGRCGTya6fNJ5TOoutOHoXqZ9I",
+    authDomain: "where-swaldo-eb0d8.firebaseapp.com",
+    projectId: "where-swaldo-eb0d8",
+    storageBucket: "where-swaldo-eb0d8.appspot.com",
+    messagingSenderId: "737753416238",
+    appId: "1:737753416238:web:bb5f22c4200bca34a8d5bf"
+  };
+
+const app = initializeApp(firebaseConfig)
